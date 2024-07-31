0dbd9ad08807b6b1510df9052f71d856b17460a6
Sahar Ghadampoor Hafezi <saharghadampoor@gmail.com>
2024-07-31 19:52:16 +0330

    implement company list page

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import routerBindings, {
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { Home, ForgotPassword, Register, Login } from "./pages";
+import { Home, ForgotPassword, Register, Login, CompanyList, Create } from "./pages";
 import { Layout } from "./components/layout";
 import { resources } from "./config/resources";
 
@@ -61,6 +61,10 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+                  <Route path="/companies">
+                  <Route index element={<CompanyList />} />
+                  <Route path="new" element={<Create />} />
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
